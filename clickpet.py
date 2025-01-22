69d140a0c29f8abec6ecd7345e8841fd7210ef2c
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-22 07:03:20 -0600

    Update clickpet.py

--- clickpet.py
+++ clickpet.py
@@ -2,3 +2,9 @@ import streamlit as st
 clicks=0
 st.title("Online Clickpet")
 st.info("This website will not save progress!")
+
+if st.button("Click!"):
+  clicks+=1
+  break
+
+st.write("You have clicked",clicks,"times!" )
