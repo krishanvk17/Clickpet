f6b53013a1ea6c9f47439552b0f5fe59e706692c
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-01-22 07:29:19 -0600

    Update clickpet.py

--- clickpet.py
+++ clickpet.py
@@ -4,7 +4,7 @@ st.title("Online Clickpet")
 st.info("This website will not save progress!")
 
 if st.button("Click!"):
-  clicks+=1
+  clicks=clicks+1
 
   
   st.write("You have clicked",clicks,"times!" )
